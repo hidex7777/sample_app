34f88c5c9e585cd18c828578f5e5ea0f420c2bc0
Hideo Saito <hidex7777@gmail.com>
2014-06-25 23:45:09 +0900

    finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,10 +1,10 @@
 include ApplicationHelper
 
 def sign_in(user, options={})
-	if otions[:no_capybara]
+	if options[:no_capybara]
 		remember_token = User.new_remember_token
 		cookies[:remember_token] = remember_token
-		user.update_attribute(:remember_token, User.encrypt(remamber_token))
+		user.update_attribute(:remember_token, User.encrypt(remember_token))
 	else
 		visit signin_path
 		fill_in "Email",			with: user.email
